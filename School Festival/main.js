8a9c00b2472d12c47839b11b7f40da45f9936b07
tadryo <49946770+tadryo@users.noreply.github.com>
2023-08-29 21:56:15 +0900

    Update main.js

--- School Festival/main.js
+++ School Festival/main.js
@@ -2,5 +2,6 @@ let name = document.getElementById('name');
 let score = document.getElementById('score');
 let text = document.getElementById('text');
 function butotnClick(){
+  console.log(name.value);
   text.innerText = '名前：' + name.value + '　スコア：' + score.value;
 }
