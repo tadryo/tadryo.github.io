08abdc91413b8339a0086952927afdadeebe55f3
tadryo <49946770+tadryo@users.noreply.github.com>
2023-08-31 14:57:10 +0900

    Update main.js

--- School Festival/main.js
+++ School Festival/main.js
@@ -6,16 +6,17 @@ function butotnClick(){
   first = document.getElementById('first');
   second = document.getElementById('second');
   third = document.getElementById('third');
-  console.log(score1.value);
-  console.log(score2.value);
   if (max < score2.value) {
     max = score2.value;
     first.innerText = '名前：' + score1.value + '　スコア：' + score2.value;
+    console.log(max,middle,min);
   } else if (middle < score2.value) {
     middle = score2.value;
     second.innerText = '名前：' + score1.value + '　スコア：' + score2.value;
+    console.log(max,middle,min);
   } else if (min < score2.value) {
     min = score2.value;
     third.innerText = '名前：' + score1.value + '　スコア：' + score2.value;
+    console.log(max,middle,min);
   }
 }
