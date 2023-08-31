9ceca6e045ff1b91fae1c2c1980849194818661d
tadryo <49946770+tadryo@users.noreply.github.com>
2023-08-31 13:59:43 +0900

    Update main.js

--- School Festival/main.js
+++ School Festival/main.js
@@ -2,7 +2,7 @@ function butotnClick(){
   score1 = document.getElementById('score1');
   score2 = document.getElementById('score2');
   text = document.getElementById('text');
-  console.log(score1);
-  console.log(score2);
-  text.innerText = 'スコア1：' + score1.value + '　スコア2：' + scor2.value;
+  console.log(score1.value);
+  console.log(score2.value);
+  text.innerText = 'スコア1：' + score1.value + '　スコア2：' + score2.value;
 }
