c3f854e6bd14d5475e0ac04d5912c5d176d33ab7
tadryo <49946770+tadryo@users.noreply.github.com>
2023-08-31 17:52:10 +0900

    Update main.js

--- School Festival/main.js
+++ School Festival/main.js
@@ -1,4 +1,5 @@
 var max = 0, middle = 0, min = 0;
+var max_name = "？？？？？", middle_name = "？？？？？", min_name = "？？？？？";
 
 function butotnClick(){
   score1 = document.getElementById('score1');
@@ -7,13 +8,19 @@ function butotnClick(){
   second = document.getElementById('second');
   third = document.getElementById('third');
   if (max < parseInt(score2.value)) {
-    max = parseInt(score2.value);
     first.innerText = score1.value + '　スコア：' + score2.value;
+    second.innerText = max_name + '　スコア：' + max;
+    third.innerText = middle_name + '　スコア：' + middle;
+    max = parseInt(score2.value);
+    max_name = score1.value;
   } else if (middle < parseInt(score2.value)) {
-    middle = parseInt(score2.value);
     second.innerText = score1.value + '　スコア：' + score2.value;
+    third.innerText = min_name + '　スコア：' + middle;
+    middle = parseInt(score2.value);
+    middle_name = score1.value;
   } else if (min < parseInt(score2.value)) {
-    min = parseInt(score2.value);
     third.innerText = score1.value + '　スコア：' + score2.value;
+    min = parseInt(score2.value);
+    min_name = score1.value;
   }
 }
